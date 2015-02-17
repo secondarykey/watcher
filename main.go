7f83270e48048c3a90ca884902c7fada0f3f4e4d
secondarykey <secondarykey@gmail.com>
2015-02-17 17:36:02 +0900

    initialize

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"./watcher"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var version string
+var show_version = flag.Bool("version", false, "show version")
+var Usage = func() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
+func main() {
+
+	path, _ := os.Getwd()
+
+	var target string
+	var duration int
+	var ignore string
+
+	patrol := flag.Bool("patrol", false, "Show Patrol TimeStamp")
+	flag.StringVar(&target, "target", path, "Search path")
+	flag.IntVar(&duration, "duration", 10, "Search duration")
+	flag.StringVar(&ignore, "ignore", "cache", "Search path")
+	flag.Parse()
+
+	args := flag.Args()
+	if *show_version {
+		fmt.Printf("version: %s\n", version)
+		return
+	}
+
+	if len(args) != 1 {
+		fmt.Println("Arg Error")
+		return
+	}
+
+	watcher.Run(target, ignore, args[0], duration, *patrol)
+}
---
+++ watcher/watcher.go
@@ -0,0 +1,104 @@
+package watcher
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+)
+
+var targetFiles map[string]os.FileInfo
+var mode = true
+var spike chan string
+
+func Run(target, ignore, cmd string, duration int, patrol bool) {
+
+	fmt.Println("Watcher Start")
+	targetFiles = make(map[string]os.FileInfo)
+	spike = make(chan string)
+
+	err := search(target)
+	if err != nil {
+		panic(err)
+	}
+
+	cmds := strings.Split(cmd, " ")
+	for {
+		select {
+		case t1 := <-time.After(time.Duration(duration) * time.Second):
+			if patrol {
+				fmt.Println(t1)
+			}
+			mode = false
+			go search(target)
+		case triger := <-spike:
+			match, _ := regexp.MatchString(ignore, triger)
+			if !match {
+				fmt.Println(triger)
+				out, _ := exec.Command(cmds[0], cmds[1:]...).CombinedOutput()
+				fmt.Println(string(out))
+			}
+		}
+	}
+}
+
+func search(rootPath string) error {
+	err := updateFileInfo(rootPath, rootPath)
+	return err
+}
+
+func listFiles(rootPath, searchPath string) error {
+
+	fis, err := ioutil.ReadDir(searchPath)
+	if err != nil {
+		return err
+	}
+
+	for _, fi := range fis {
+		fullPath := filepath.Join(searchPath, fi.Name())
+		err := updateFileInfo(rootPath, fullPath)
+		if err != nil {
+			return nil
+		}
+	}
+	return nil
+}
+
+func updateFileInfo(rootPath, path string) error {
+
+	fInfo, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	if isSpike(path, fInfo) {
+		mode = true
+		spike <- path
+	}
+
+	// fileinfo update
+	targetFiles[path] = fInfo
+	if fInfo.IsDir() {
+		go listFiles(rootPath, path)
+	}
+	return nil
+}
+
+func isSpike(fullPath string, fInfo os.FileInfo) bool {
+
+	if !mode {
+		src := targetFiles[fullPath]
+		if src != nil {
+			if fInfo.ModTime() != src.ModTime() {
+				return true
+			}
+		} else {
+			return true
+		}
+	}
+	return false
+}
