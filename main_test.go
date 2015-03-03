64155a9b4f90ce927eccf4b6b8ad73a12221000d
secondarykey <secondarykey@gmail.com>
2015-03-04 07:50:03 +0900

    added progress function test

--- main_test.go
+++ main_test.go
@@ -2,7 +2,45 @@ package main
 
 import (
 	"testing"
+	"time"
 )
 
-func TestMock(t *testing.T) {
+func TestRun(t *testing.T) {
+}
+
+func TestGetListDir(t *testing.T) {
+}
+
+func TestMonitor(t *testing.T) {
+}
+
+func TestNotify(t *testing.T) {
+}
+
+func TestIgnore(t *testing.T) {
+}
+
+func TestCommand(t *testing.T) {
+}
+
+func ExampleProgress() {
+
+	wait := make(chan bool)
+	go progress(wait)
+
+	val := 0
+	for {
+		select {
+		case <-time.After(time.Second * 2):
+			val++
+			if val == 3 {
+				time.Sleep(100 * time.Millisecond)
+				wait <- true
+				return
+			}
+		}
+	}
+
+	// Output:
+	// ###
 }
